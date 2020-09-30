d98dec29b5723e7afb1e6a406733293029631c45
kihyun1998 <71832733+kihyun1998@users.noreply.github.com>
2020-09-30 23:37:36 +0900

    Add files via upload

---
+++ string.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>String & String Operator</h1>
+    <?php
+    echo 'Hello world';
+    ?>
+    <h2>concatenation operator</h2>
+    <?php
+    echo "Hello "."world";
+    ?>
+  </body>
+  <h2>String length function</h2>
+  <?php
+  echo strlen("Hello World");
+  ?>
+</html>
---
+++ variable.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $a = 10;
+    echo $a+1;
+    ?>
+    <?php
+    $name = " 12까지 봤다. ";
+    echo "Lorem ipsum dolor sit amet,".$name."
+    consectetur adipisicing elit, sed do
+    eiusmod tempor incididunt ut labore et
+    dolore magna aliqua. Ut enim ad ".$name."minim veniam,
+    quis nostrud exercitation ".$name."ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor ".$name."in reprehenderit in voluptate
+    velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt
+    in culpa qui officia deserunt mollit anim id est laborum.";
+    ?>
+  </body>
+</html>
